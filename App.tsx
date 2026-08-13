4bf8d17fae01f8bc702bfff11617be1af75a513d
lydia205 <lydiaomombo35@gmail.com>
2026-08-13 07:22:45 -0700

    added container and setImage

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput , Button, Image,SafeAreaView, ScrollView, Animated, ViewStyle, StyleProp} from 'react-native';
+import { StyleSheet, Text, View, TextInput , Button, Image,SafeAreaView, ScrollView, Animated, ViewStyle, StyleProp,ImageSourcePropType} from 'react-native';
 import React, { useState , useRef, useEffect, ReactNode} from 'react';
 import {NavigationContainer}from '@react-navigation/native';
 import {createNativeStackNavigator}from '@react-navigation/native-stack';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RadioButton, shadow } from 'react-native-paper';
 
 
 function isEmpty(Value: any){
@@ -127,12 +128,85 @@ function MainScreen ({navigation} :MainScreenProps){
 
         const NameGet= route.params.FirstNameSend;
         const SurnameGet= route.params.SurnameSend;
+        const[selectedValue, setSelectedValue] = useState('0');
+        const[ImageBlock, setImage]= useState<ImageSourcePropType | undefined>(undefined);
 
         return (
 
           <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Name: {NameGet} Surname: {SurnameGet}</Text>
+            <View style={{ flex: 0, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{ fontWeight: 'bold', fontSize: 20}}>
+              Hello {NameGet} {SurnameGet} !</Text>
+            <Text>please select a language:</Text>
+            </View>
+
+            <View style={styles.RadioContainer}>
+              <View style={styles.radioGroupe}>
+                <View style={styles.radioButton}>
+                  <RadioButton.Android
+                  value='1'
+                  status={selectedValue == '1'? 'checked': 'unchecked'}
+
+                    onPress={() => setSelectedValue ('1')}
+                    color='#007BFF'
+                    />
+                    <Text style={styles.radiolabel}>React Native</Text>
+                </View>
+                <View style={styles.radioButton}>
+                  <RadioButton.Android
+                  value='2'
+                  status={selectedValue == '2'? 'checked': 'unchecked'}
+
+                    onPress={() => setSelectedValue ('2')}
+                    color='#ba97b3'
+                    />
+                    <Text style={styles.radiolabel}>Kotlin</Text>
+                </View>
+                <View style={styles.radioButton}>
+                   <RadioButton.Android
+                  value='3'
+                  status={selectedValue == '3'? 'checked': 'unchecked'}
+
+                    onPress={() => setSelectedValue ('3')}
+                    color='#4f484e'
+                    />
+                    <Text style={styles.radiolabel}>HTML and CSS</Text>
+                </View>
+        
+                </View>
+              </View>
+
+            
+            <View style={{flex: 1}}>
+                  <Text style={{fontWeight: "bold", flex:0, paddingTop:30,
+                    justifyContent:'center', textAlign: 'center', alignItems:'center'
+              
+                  }}>
+                  your chosen language:
+                  </Text>
+                  <Button title="Click !"
+                  onPress={() =>{
+                    switch(selectedValue){
+                      case "1":
+                        setImage (require('./_image/photo.jpg'));
+                        break;
+                        case "2":
+                          setImage (require('./_image/photo.jpg'))
+                          break;
+                          case "3":
+                            setImage(require('./_image/photo.jpg'));
+                            break;
+                            default:
+                              setImage(undefined);
+                    }
+                  }}/>
+
+               <View style={styles.container}>
+                <Image source={ImageBlock} style={styles.viewImage}></Image>
+                </View>   
+          </View>
          </View>   
+
         );
       
       }
@@ -216,7 +290,67 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   }, 
   blank: {
+    fontSize :0,
+  },
+
+  red: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 26,
+    textAlign: 'center'
+
+  },
   
+ RadioContainer: {
+  flex: 0, 
+  backgroundColor:'#F5F5F5',
+  justifyContent: 'center',
+  alignItems:'center',
+  
+ },
+ radioGroupe:{
+  flexDirection:'row',
+  alignItems:'center',
+  justifyContent:'space-around',
+  marginTop: 20,
+  borderRadius:8,
+  backgroundColor:'white',
+  padding:16,
+  elevation:4,
+  shadowColor: '#000',
+  shadowOffset: {
+    width:0,
+     height:2,
+  },
+  shadowOpacity:0.25,
+  shadowRadius:3.84,
+  
+ },
+ radioButton:{
+  flexDirection:'row',
+  alignItems:'center',
+  
+ },
+ radiolabel:{
+  marginLeft: 8,
+  fontSize: 16,
+  color:'#333'
+
+ } ,
+  container: {
+    flex: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+
+  viewImage:{
+    width:350,
+    height:350,
+    alignContent:'center'
   }
+
+   
+}
   
-});
+);
